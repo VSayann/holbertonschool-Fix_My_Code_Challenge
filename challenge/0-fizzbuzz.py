23b9b4e96cede3a5a258450747c6731f2ad8db67
SVKen <sayann.valmond@gmail.com>
2025-05-14 18:10:04 +0200

    Remove error handling for non-integer input in FizzBuzz script

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -34,9 +34,5 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    try:
-        number = int(sys.argv[1])
-        fizzbuzz(number)
-    except ValueError:
-        print("Invalid number. Please enter an integer.")
-        sys.exit(1)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
