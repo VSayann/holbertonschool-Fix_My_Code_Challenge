a1d69ab96168b56f8241090e0b705a2d6d4712f6
SVKen <sayann.valmond@gmail.com>
2025-05-17 15:43:46 +0200

    Refactor delete_dnodeint_at_index function for improved clarity and consistency in comments

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,35 +2,44 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * delete_dnodeint_at_index - Deletes the node at index index of a
+ *                            doubly linked list
+ * @head: Pointer to the pointer to the head of the list
+ * @index: Index of the node to delete (starting from 0)
  *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if it succeeded, -1 if it failed
  */
- int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
- {
-     dlistint_t *current = *head;
-     unsigned int i;
- 
-     if (!head || !(*head))
-         return (-1);
- 
-     for (i = 0; current && i < index; i++)
-         current = current->next;
- 
-     if (!current)
-         return (-1);
- 
-     if (current->prev)
-         current->prev->next = current->next;
-     else
-         *head = current->next;
- 
-     if (current->next)
-         current->next->prev = current->prev;
- 
-     free(current);
-     return (1);
- }
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+
+    for (i = 0; current != NULL && i < index; i++)
+    {
+        current = current->next;
+    }
+
+    if (current == NULL)
+        return (-1);
+
+    if (current->prev == NULL)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        current->prev->next = current->next;
+        if (current->next != NULL)
+            current->next->prev = current->prev;
+    }
+
+    free(current);
+    return (1);
+}
